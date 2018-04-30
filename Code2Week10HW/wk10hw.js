5a75edb3c9e38495d139ea0102e16f8589e96164
pavec <31394711+pavec@users.noreply.github.com>
2018-04-29 21:24:27 -0400

    adasas

--- Code2Week10HW/wk10hw.js
+++ Code2Week10HW/wk10hw.js
@@ -1,29 +1,69 @@
+
+
+
+// function setup() {
+//   createCanvas(400, 400);
+
+
+// }
+// function draw() {
+//   background(50);
+//   for (var i = 0; i < allGifs.length; i++) {
+//     allGifs[i].addSpeed(0.1, 90);
+//     if (allGifs[i].position.y > height) {
+//       allGifs[i].velocity.y *= -1;
+//     }
+//     if (allGifs[i].position.x > width ||
+//         allGifs[i].position.x < 0) {
+//       allGifs[i].remove();
+//     }
+//   }
+//   textAlign(RIGHT, TOP);
+//   text("sprite count: " + allGifs.length,
+//     width-10, 10);
+//   drawGifs();
+// }
+// function mousePressed() {
+//   var gif = createSprite(width/2, height/2,
+//     random(10, 50), random(10, 50));
+//   gif.Color = color(255);
+//   gif.velocity.y = random(3);
+//   gif.velocity.x = random(-3, 3);
+//   gif.position.x = mouseX;
+//   gif.position.y = mouseY;
+// }
+
+
+CREATING NEW BOUNCING SPRITES ON CLICK
 function setup() {
   createCanvas(400, 400);
 }
 function draw() {
   background(50);
-  for (var i = 0; i < allGifs.length; i++) {
-    allGifs[i].addSpeed(0.1, 90);
-    if (allGifs[i].position.y > height) {
-      allGifs[i].velocity.y *= -1;
+  for (var i = 0; i < allSprites.length; i++) {
+    // gravity
+    allSprites[i].addSpeed(0.1, 90);
+    if (allSprites[i].position.y > height) {
+      allSprites[i].velocity.y *= -1;
     }
-    if (allGifs[i].position.x > width ||
-        allGifs[i].position.x < 0) {
-      allGifs[i].remove();
+    // any code that removes sprites should be
+    // the *last* thing in the loop!
+    if (allSprites[i].position.x > width ||
+        allSprites[i].position.x < 0) {
+      allSprites[i].remove();
     }
   }
   textAlign(RIGHT, TOP);
-  text("sprite count: " + allGifs.length,
+  text("sprite count: " + allSprites.length,
     width-10, 10);
-  drawGifs();
+  drawSprites();
 }
 function mousePressed() {
-  var gif = createSprite(width/2, height/2,
+  var spr = createSprite(width/2, height/2,
     random(10, 50), random(10, 50));
-  gif.Color = color(255);
-  gif.velocity.y = random(3);
-  gif.velocity.x = random(-3, 3);
-  gif.position.x = mouseX;
-  gif.position.y = mouseY;
+  spr.shapeColor = color(255);
+  spr.velocity.y = random(3);
+  spr.velocity.x = random(-3, 3);
+  spr.position.x = mouseX;
+  spr.position.y = mouseY;
 }
