dbfcaa894c1ab9cf98ff9d1371878233d8239aed
pavec <31394711+pavec@users.noreply.github.com>
2018-04-29 21:07:12 -0400

    min and dom

--- Code2Week10HW/wk10hw.js
+++ Code2Week10HW/wk10hw.js
@@ -3,29 +3,27 @@ function setup() {
 }
 function draw() {
   background(50);
-  for (var i = 0; i < allSprites.length; i++) {
-    allSprites[i].addSpeed(0.1, 90);
-    if (allSprites[i].position.y > height) {
-      allSprites[i].velocity.y *= -1;
+  for (var i = 0; i < allGifs.length; i++) {
+    allGifs[i].addSpeed(0.1, 90);
+    if (allGifs[i].position.y > height) {
+      allGifs[i].velocity.y *= -1;
     }
-    // any code that removes sprites should be
-    // the *last* thing in the loop!
-    if (allSprites[i].position.x > width ||
-        allSprites[i].position.x < 0) {
-      allSprites[i].remove();
+    if (allGifs[i].position.x > width ||
+        allGifs[i].position.x < 0) {
+      allGifs[i].remove();
     }
   }
   textAlign(RIGHT, TOP);
-  text("sprite count: " + allSprites.length,
+  text("sprite count: " + allGifs.length,
     width-10, 10);
-  drawSprites();
+  drawGifs();
 }
 function mousePressed() {
-  var spr = createSprite(width/2, height/2,
+  var gif = createSprite(width/2, height/2,
     random(10, 50), random(10, 50));
-  spr.shapeColor = color(255);
-  spr.velocity.y = random(3);
-  spr.velocity.x = random(-3, 3);
-  spr.position.x = mouseX;
-  spr.position.y = mouseY;
+  gif.Color = color(255);
+  gif.velocity.y = random(3);
+  gif.velocity.x = random(-3, 3);
+  gif.position.x = mouseX;
+  gif.position.y = mouseY;
 }
